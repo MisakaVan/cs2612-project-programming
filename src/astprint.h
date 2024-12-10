c6c6f7121bf66e51c25778e908aee7c3086d7783
MisakaVan <2102315149@qq.com>
2024-12-10 13:33:20 +0800

    feat(astprint): add support to select different var_decl_expr printers

--- src/astprint.h
+++ src/astprint.h
@@ -26,4 +26,15 @@ void astprint_var_decl_expr(struct var_decl_expr* e);
 // * bar[10] -> `bar` is array[10] of pointer
 void astprint_var_decl_expr_backtrace(struct var_decl_expr* e);
 
+// print both the bottom-up and top-down AST of a var_decl_expr.
+void astprint_var_decl_expr_both(struct var_decl_expr* e);
+
+enum VarDeclExprPrinter {
+    VAR_DECL_EXPR_PRINTER_AST,
+    VAR_DECL_EXPR_PRINTER_BACKTRACE,
+    VAR_DECL_EXPR_PRINTER_BOTH
+};
+
+void set_var_decl_expr_printer(enum VarDeclExprPrinter p);
+
 #endif // ASTPRINT_H_INCLUDED
