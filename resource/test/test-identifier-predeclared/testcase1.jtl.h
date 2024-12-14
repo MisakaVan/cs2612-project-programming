3d553a9ada201d16b1b6369fa5a411bccfbf3221
MisakaVan <2102315149@qq.com>
2024-12-15 03:00:44 +0800

    fix(test): update typedef warning message to suggest using `struct Foo`

--- resource/test/test-identifier-predeclared/testcase1.jtl.h
+++ resource/test/test-identifier-predeclared/testcase1.jtl.h
@@ -7,7 +7,7 @@ enum Foo b; // Warning: Identifier Foo has been registered as struct, not enum
 union Foo c; // Warning: Identifier Foo has been registered as struct, not union
 
 
-typedef Foo foo_t;
+typedef Foo foo_t; // warning: should use `struct Foo` instead of `Foo`
 foo_t d; // OK: foo_t has been registered as typedef
 bar_t e; // Warning: Identifier bar_t has never been registered
 
